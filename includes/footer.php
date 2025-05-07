0d323305c4df07976725c984f6dd1711c32d68a5
Bonkiniato // GM <145768219+Bonkiniato@users.noreply.github.com>
2025-05-07 16:25:24 +0200

    license is now on pastebin

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-    <footer class="page-footer"><a href="https://github.com/gocsaldev/libradmin/blob/main/LICENSE">Copyright © 2025 Gocsál Mátyás</a></footer>
+    <footer class="page-footer"><a href="https://pastebin.com/EKsxnf2v">Copyright © 2025 Gocsál Mátyás</a></footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
